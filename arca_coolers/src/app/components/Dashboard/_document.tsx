ee631a4bee647800b2836e78a2b223bb3ae44ed7
Samira Hazim <samirahazimr@gmail.com>
2025-06-14 21:25:35 -0600

    Arreglando errores de deploy

--- arca_coolers/src/app/components/Dashboard/_document.tsx
+++ arca_coolers/src/app/components/Dashboard/_document.tsx
@@ -1,15 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="es">
       <Head>
-        <script
-          src="https://public.tableau.com/javascripts/api/tableau-2.min.js"
-          type="text/javascript"
-        ></script>
+        {/* Aquí no se coloca nada más */}
       </Head>
       <body>
+        {/* Carga asincrónica de la API de Tableau */}
+        <Script
+          src="https://public.tableau.com/javascripts/api/tableau-2.min.js"
+          strategy="beforeInteractive"
+        />
         <Main />
         <NextScript />
       </body>
